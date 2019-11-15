2e3f2fe2ca20f66685b3ef9e7181cdf2943646a6
Leelsmuth <femianjorin@rocketmail.com>
2019-11-15 10:03:21 +0100

    implemented the login and register functionality, implemented the authenticate middleware using jwt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,12 +1,17 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
 
-router.post('/register', (req, res) => {
+const Users = require("../helpers/users-model");
+
+router.post("/register", (req, res) => {
   // implement registration
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
-  user.password = hash;
+  let { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10); // 2 ^ n
+  password = hash;
 
-  Users.add(user)
+  Users.add({ username, password })
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -15,8 +20,38 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        // THIS HERE IS THE PLACE TO MAKE THE TOKEN
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
